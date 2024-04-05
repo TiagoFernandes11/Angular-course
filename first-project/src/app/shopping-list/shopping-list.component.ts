a81019c49644326e6fd129faba2ed74f38f05859
Tiago Fernandes Ribeiro <89404933+TiagoFernandes11@users.noreply.github.com>
2024-04-05 20:32:04 -0300

    Ingredient model and shopping list template

--- first-project/src/app/shopping-list/shopping-list.component.ts
+++ first-project/src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrl: './shopping-list.component.css'
+  styleUrl: './shopping-list.component.css',
 })
 export class ShoppingListComponent {
-
+  ingredients = [
+    new Ingredient('The Son', 1),
+    new Ingredient('The Father', 1),
+    new Ingredient('The Spirit', 1),
+  ];
 }
